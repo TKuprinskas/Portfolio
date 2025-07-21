017af2affdc4042e205984148622d8419b768d6c
Tomas Kuprinskas <t.kuprinskas@gmail.com>
2025-07-21 12:39:00 +0300

    added new project

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -12,8 +12,8 @@ const Hero = () => (
         My Personal Portfolio
       </SectionTitle>
       <SectionText>
-        My name is Tomas Kuprinskas and I am junior full stack web developer, I have a passion for web development and I am currently working as a freelancer
-        and I am always looking for new challenges and opportunities.
+        My name is Tomas Kuprinskas and I am full stack web developer, I have a passion for web development and I am currently working as a freelancer and I am
+        always looking for new challenges and opportunities.
       </SectionText>
       <Button onClick={() => window.open('https://tkuprinskas.lt/MyResume/Tomas%20Kuprinskas%20CV%20-%20Frontend%20Developer.pdf', '_blank')}>My Resume</Button>
     </LeftSection>
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,43 +1,69 @@
-import React from 'react';
+import React from "react";
 
-import { BlogCard, CardInfo, ExternalLinks, GridContainer, HeaderThree, Hr, Tag, TagList, TitleContent, UtilityList, Img } from './ProjectsStyles';
-import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
-import { projects } from '../../constants/constants';
+import {
+  BlogCard,
+  CardInfo,
+  ExternalLinks,
+  GridContainer,
+  HeaderThree,
+  Hr,
+  Tag,
+  TagList,
+  TitleContent,
+  UtilityList,
+  Img,
+} from "./ProjectsStyles";
+import {
+  Section,
+  SectionDivider,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import { projects } from "../../constants/constants";
 
 const Projects = () => (
-  <Section nopadding id='projects'>
+  <Section nopadding id="projects">
     <SectionDivider />
     <SectionTitle main>Projects</SectionTitle>
     <GridContainer>
-      {projects.map(({ id, image, title, description, tags, website, visit }) => (
-        <BlogCard key={id}>
-          <Img src={image} alt={title} />
-          <TitleContent>
-            <HeaderThree title='true'>{title}</HeaderThree>
-            <Hr />
-          </TitleContent>
-          <CardInfo>{description}</CardInfo>
-          <div>
-            <br />
-            <TitleContent>Stack</TitleContent>
-            <TagList>
-              {tags.map((tag, i) => (
-                <Tag key={i}>{tag}</Tag>
-              ))}
-            </TagList>
-          </div>
-          <UtilityList>
-            {visit && (
-              <ExternalLinks href={visit.link} target='_blank'>
-                {visit.title}
+      {projects.map(
+        ({ id, image, title, description, tags, website, visit }) => (
+          <BlogCard key={id}>
+            <Img src={image} alt={title} />
+            <TitleContent>
+              <HeaderThree title="true">{title}</HeaderThree>
+              <Hr />
+            </TitleContent>
+            <CardInfo>{description}</CardInfo>
+            <div>
+              <br />
+              <TitleContent>Stack</TitleContent>
+              <TagList>
+                {tags.map((tag, i) => (
+                  <Tag key={i}>{tag}</Tag>
+                ))}
+              </TagList>
+            </div>
+            <UtilityList>
+              {visit && (
+                <ExternalLinks
+                  href={visit.link}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {visit.title}
+                </ExternalLinks>
+              )}
+              <ExternalLinks
+                href={website}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Website
               </ExternalLinks>
-            )}
-            <ExternalLinks href={website} target='_blank'>
-              Website
-            </ExternalLinks>
-          </UtilityList>
-        </BlogCard>
-      ))}
+            </UtilityList>
+          </BlogCard>
+        )
+      )}
     </GridContainer>
   </Section>
 );
